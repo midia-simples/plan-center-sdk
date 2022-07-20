17e4a9adf2cea75a9edb163675934e73a5b3a057
Matheus Gomes <mathvicentin@gmail.com>
2022-07-20 15:20:12 -0300

    Release - 0.3.2 (#16)
    * feat: adicionado consultas para canais e combos (#15)

---
+++ src/Services/Combo.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MidiaSimples\PlanCenterSDK\Services;
+
+use Illuminate\Http\Client\PendingRequest;
+use MidiaSimples\PlanCenterSDK\Contracts\Services\ComboRepositoryInterface;
+use MidiaSimples\PlanCenterSDK\Repository;
+
+class Combo extends Repository implements ComboRepositoryInterface
+{
+    /**
+     * @var \Illuminate\Http\Client\PendingRequest
+     */
+    protected PendingRequest $client;
+
+    public function __construct($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    public function all(array $options = []): array
+    {
+        return $this->getRequest('combos', $options);
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    public function paginate(array $options = []): array
+    {
+        return $this->getRequest("combos/paginate", $options);
+    }
+
+    /**
+     * @param $id
+     * @return array
+     */
+    public function show($id): array
+    {
+        return $this->getRequest("combos/{$id}");
+    }
+}
